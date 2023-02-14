7c9aebd4e492a4a6ef3fd1d9f6283d1e13b6542a
hb9397@naver.com <hb9397@naver.com>
2023-02-14 17:35:14 +0900

    saramara-community-MigrationTest 20220209 1. Exception 반환 객체 수정 -> 일부 해결 불가 - Message 수정 불가 -> /api/authenticate HttpResponse.sendError 에 대한 trace 제거 불가, /api/signup 의 409 에 대한 Message 삽입 불가
    - Client 에서 Http 요청, path, error 를 보고 판단 필요
    
    2. OAuth 2 + JWT 결합 중

--- src/main/java/com/kakao/saramaracommunity/config/SecurityConfig.java
+++ src/main/java/com/kakao/saramaracommunity/config/SecurityConfig.java
@@ -43,7 +43,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
+        /*httpSecurity
+            .cors()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음
+            .and()
+            .csrf().disable() // csrf 미사용
+            .headers().frameOptions().disable()
+            .and()
+            .formLogin().disable() // 로그인 폼 미사용
+            .httpBasic().disable() // Http basic Auth 기반으로 로그인 인증창이 열림(disable 시 인증창 열리지 않음)
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint) // 인증,인가가 되지 않은 요청 시 발생
+            .and()
+            .oauth2Login()
+            .authorizationEndpoint().baseUri("/oauth2/authorization") // 소셜 로그인 Url
+            .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository()) // 인증 요청을 쿠키에 저장하고 검색
+            .and()
+            .redirectionEndpoint().baseUri("/oauth2/callback/*") // 소셜 인증 후 Redirect Url
+            .and()
+            .userInfoEndpoint().userService(customOAuth2UserService) // 소셜의 회원 정보를 받아와 가공처리
+            .and()
+            .successHandler(oAuth2AuthenticationSuccessHandler) // 인증 성공 시 Handler
+            .failureHandler(oAuth2AuthenticationFailureHandler); // 인증 실패 시 Handler*/
 
         httpSecurity
                 // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
